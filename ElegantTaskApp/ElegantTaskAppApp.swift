3a0d2d7a766b889569bbf0c2a0bb78c9a2963cc2
hanyong.lee <tidyrain@gmail.com>
2022-11-02 22:59:24 +0900

    Initial Commit

---
+++ ElegantTaskApp/ElegantTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElegantTaskAppApp.swift
+//  ElegantTaskApp
+//
+//  Created by Hanyong Lee on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct ElegantTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
